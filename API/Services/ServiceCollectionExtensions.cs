49394d168a4dba3b6a5b756190cd62992ee84cfe
Ryan Flores <rryanflorres@gmail.com>
2025-09-27 21:29:55 -0700

    Cleanup program.cs

---
+++ API/Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Portfolio.Shared.Services;
+using StackExchange.Redis;
+
+namespace API.Services;
+
+public static class ServiceCollectionExtensions {
+    public static void AddCorsPolicies(this IServiceCollection services) {
+        services.AddCors(options => {
+            options.AddPolicy("dev-policy", policy =>
+                policy.WithOrigins("http://localhost:4200", "http://localhost:30082")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+        });
+
+        services.AddCors(options => {
+            options.AddPolicy("prod-policy", policy =>
+                policy.WithOrigins("http://portfolio-frontend-service", "https://ryanflores.dev")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+        });
+    }
+
+    public static void AddProjectDependencies(this IServiceCollection services,
+        ConfigurationManager configuration) {
+        services.AddStackExchangeRedisCache(options => {
+            options.Configuration = configuration.GetConnectionString("Redis") ?? "localhost:6379";
+        });
+
+        services.AddSingleton<IConnectionMultiplexer>(_ =>
+            ConnectionMultiplexer.Connect(configuration.GetConnectionString("Redis") ?? "localhost:6379"));
+
+
+        services.AddSingleton<RedisService>();
+        services.AddSingleton<GitHubCommitService>();
+        services.AddSingleton<CommitAnalysisService>();
+    }
+}
