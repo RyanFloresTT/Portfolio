c43d4aaaf82e490bd6c378129498852a13fedab4
Ryan Flores <rryanflorres@gmail.com>
2025-09-28 13:09:57 -0700

    Add StackExchangeRedis for SignalR/API Replicas

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,4 @@
 using API.Services;
-using Microsoft.AspNetCore.SignalR;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -11,27 +10,17 @@ builder.Services.AddHttpClient();
 
 builder.Services.AddProjectDependencies(builder.Configuration);
 
-builder.Services.AddSignalR(options => {
-    options.EnableDetailedErrors = true;
-    options.KeepAliveInterval = TimeSpan.FromSeconds(15);
-    options.ClientTimeoutInterval = TimeSpan.FromSeconds(30);
-});
+builder.Services.AddSignalR().AddStackExchangeRedis(builder.Configuration["ConnectionStrings:Redis"] ?? string.Empty);
 
 builder.Services.AddCorsPolicies();
 
 WebApplication app = builder.Build();
 
-if (app.Environment.IsDevelopment()) {
-    app.MapOpenApi();
-}
+if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
-if (app.Environment.IsDevelopment()) {
-    app.UseCors("dev-policy");
-} else {
-    app.UseCors("prod-policy");
-}
+app.UseCors(app.Environment.IsDevelopment() ? "dev-policy" : "prod-policy");
 
 app.UseRouting();
 
